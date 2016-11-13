4f0ac2c79f90d88021ed3ff9b1ce41fa000e062c
Dominik Zogg <dominik.zogg@gmail.com>
2016-11-13 20:12:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# chubbyphp-model-doctrine-dbal
+
+[![Build Status](https://api.travis-ci.org/chubbyphp/chubbyphp-model-doctrine-dbal.png?branch=master)](https://travis-ci.org/chubbyphp/chubbyphp-model-doctrine-dbal)
+[![Total Downloads](https://poser.pugx.org/chubbyphp/chubbyphp-model-doctrine-dbal/downloads.png)](https://packagist.org/packages/chubbyphp/chubbyphp-model-doctrine-dbal)
+[![Latest Stable Version](https://poser.pugx.org/chubbyphp/chubbyphp-model-doctrine-dbal/v/stable.png)](https://packagist.org/packages/chubbyphp/chubbyphp-model-doctrine-dbal)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/chubbyphp/chubbyphp-model-doctrine-dbal/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/chubbyphp/chubbyphp-model-doctrine-dbal/?branch=master)
+
+## Description
+
+## Requirements
+
+ * php: ~7.0
+ * chubbyphp/chubbyphp-model: ~2.0@dev
+ * doctrine/dbal: ^2.5.5
+
+## Installation
+
+Through [Composer](http://getcomposer.org) as [chubbyphp/chubbyphp-model-doctrine-dbal][1].
+
+## Usage
+
+[1]: https://packagist.org/packages/chubbyphp/chubbyphp-model-doctrine-dbal
+
+## Copyright
+
+Dominik Zogg 2016
---
+++ src/AbstractDoctrineRepository.php
@@ -0,0 +1,221 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Model\Doctrine\DBAL;
+
+use Chubbyphp\Model\Cache\ModelCache;
+use Chubbyphp\Model\Cache\ModelCacheInterface;
+use Chubbyphp\Model\ModelInterface;
+use Chubbyphp\Model\RepositoryInterface;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+abstract class AbstractDoctrineRepository implements RepositoryInterface
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var ModelCacheInterface
+     */
+    private $cache;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param Connection               $connection
+     * @param ModelCacheInterface|null $cache
+     * @param LoggerInterface|null     $logger
+     */
+    public function __construct(
+        Connection $connection,
+        ModelCacheInterface $cache = null,
+        LoggerInterface $logger = null
+    ) {
+        $this->connection = $connection;
+        $this->cache = $cache ?? new ModelCache();
+        $this->logger = $logger ?? new NullLogger();
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return ModelInterface|null
+     */
+    public function find(string $id)
+    {
+        $modelClass = $this->getModelClass();
+
+        $this->logger->info('model: find model {model} with id {id}', ['model' => $modelClass, 'id' => $id]);
+
+        if ($this->cache->has($id)) {
+            return $this->fromRow($this->cache->get($id));
+        }
+
+        $qb = $this->connection->createQueryBuilder();
+        $qb->select('*')->from($this->getTable())->where($qb->expr()->eq('id', ':id'))->setParameter('id', $id);
+
+        $row = $qb->execute()->fetch(\PDO::FETCH_ASSOC);
+        if (false === $row) {
+            $this->logger->warning(
+                'model: model {model} with id {id} not found',
+                ['model' => $modelClass, 'id' => $id]
+            );
+
+            return null;
+        }
+
+        $this->cache->set($row['id'], $row);
+
+        return $this->fromRow($row);
+    }
+
+    /**
+     * @param array $criteria
+     *
+     * @return null|ModelInterface
+     */
+    public function findOneBy(array $criteria)
+    {
+        $modelClass = $this->getModelClass();
+
+        $this->logger->info(
+            'model: find model {model} with criteria {criteria}',
+            ['model' => $modelClass, 'criteria' => $criteria]
+        );
+
+        $qb = $this->getFindByQueryBuilder($criteria)->setMaxResults(1);
+
+        $row = $qb->execute()->fetch(\PDO::FETCH_ASSOC);
+        if (false === $row) {
+            $this->logger->warning(
+                'model: model {model} with criteria {criteria} not found',
+                ['model' => $modelClass, 'criteria' => $criteria]
+            );
+
+            return null;
+        }
+
+        $this->cache->set($row['id'], $row);
+
+        return $this->fromRow($row);
+    }
+
+    /**
+     * @param array $criteria
+     *
+     * @return ModelInterface[]|array
+     */
+    public function findBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null): array
+    {
+        $modelClass = $this->getModelClass();
+
+        $this->logger->info(
+            'model: find model {model} with criteria {criteria}',
+            ['model' => $modelClass, 'criteria' => $criteria]
+        );
+
+        $qb = $this
+            ->getFindByQueryBuilder($criteria)
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+        ;
+
+        if (null !== $orderBy) {
+            foreach ($orderBy as $field => $direction) {
+                $qb->addOrderBy($field, $direction);
+            }
+        }
+
+        $rows = $qb->execute()->fetchAll(\PDO::FETCH_ASSOC);
+
+        if ([] === $rows) {
+            return [];
+        }
+
+        $models = [];
+        foreach ($rows as $row) {
+            $this->cache->set($row['id'], $row);
+            $models[] = $this->fromRow($row);
+        }
+
+        return $models;
+    }
+
+    /**
+     * @param array $criteria
+     *
+     * @return QueryBuilder
+     */
+    private function getFindByQueryBuilder(array $criteria = []): QueryBuilder
+    {
+        $qb = $this->connection->createQueryBuilder();
+        $qb->select('*')->from($this->getTable());
+
+        foreach ($criteria as $field => $value) {
+            $qb->andWhere($qb->expr()->eq($field, ':'.$field));
+            $qb->setParameter($field, $value);
+        }
+
+        return $qb;
+    }
+
+    /**
+     * @param ModelInterface $model
+     */
+    public function persist(ModelInterface $model)
+    {
+        $this->logger->info(
+            'model: persist model {model} with id {id}',
+            ['model' => get_class($model), 'id' => $model->getId()]
+        );
+
+        if (null === $this->find($model->getId())) {
+            $this->connection->insert($this->getTable(), $model->toRow());
+        } else {
+            $this->connection->update($this->getTable(), $model->toRow(), ['id' => $model->getId()]);
+        }
+
+        $this->cache->set($model->getId(), $model->toRow());
+    }
+
+    /**
+     * @param ModelInterface $model
+     */
+    public function delete(ModelInterface $model)
+    {
+        $this->logger->info(
+            'model: delete model {model} with id {id}',
+            ['model' => get_class($model), 'id' => $model->getId()]
+        );
+
+        $this->connection->delete($this->getTable(), ['id' => $model->getId()]);
+
+        $this->cache->remove($model->getId());
+    }
+
+    /**
+     * @param array $row
+     * @return ModelInterface
+     */
+    protected function fromRow(array $row): ModelInterface
+    {
+        /** @var ModelInterface $modelClass */
+        $modelClass = $this->getModelClass();
+
+        return $modelClass::fromRow($row);
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getTable(): string;
+}
