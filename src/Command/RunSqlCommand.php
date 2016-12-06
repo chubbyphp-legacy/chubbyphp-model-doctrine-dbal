2145131f9e1d9dc09c30ddeb0a6796d848194b09
Dominik Zogg <dominik.zogg@gmail.com>
2016-12-06 21:35:46 +0100

    simplify run sql command

--- src/Command/RunSqlCommand.php
+++ src/Command/RunSqlCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Model\Doctrine\DBAL\Command;
 
+use Doctrine\Common\Util\Debug;
 use Doctrine\DBAL\Connection;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -34,15 +35,8 @@ final class RunSqlCommand
      */
     public function __invoke(InputInterface $input, OutputInterface $output)
     {
-        if (($sql = $input->getArgument('sql')) === null) {
-            throw new \RuntimeException("Argument 'SQL' is required in order to execute this command correctly.");
-        }
-
-        $depth = $input->getOption('depth');
-
-        if (!is_numeric($depth)) {
-            throw new \LogicException("Option 'depth' must contains an integer value");
-        }
+        $sql = $this->getSql($input);
+        $depth = $this->getDepth($input);
 
         if (stripos($sql, 'select') === 0) {
             $resultSet = $this->connection->fetchAll($sql);
@@ -50,8 +44,38 @@ final class RunSqlCommand
             $resultSet = $this->connection->executeUpdate($sql);
         }
 
-        $message = \Doctrine\Common\Util\Debug::dump($resultSet, (int) $depth, true, false);
+        $message = Debug::dump($resultSet, (int) $depth, true, false);
 
         $output->write($message);
+    }
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return string
+     */
+    private function getSql(InputInterface $input): string
+    {
+        if (($sql = $input->getArgument('sql')) === null) {
+            throw new \RuntimeException("Argument 'SQL' is required in order to execute this command correctly.");
+        }
+
+        return $sql;
+    }
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return int
+     */
+    private function getDepth(InputInterface $input): int
+    {
+        $depth = $input->getOption('depth');
+
+        if (!is_numeric($depth)) {
+            throw new \LogicException("Option 'depth' must contains an integer value");
+        }
+
+        return (int) $depth;
     }
 }
